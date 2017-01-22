f2b628e59e48845d477fab8fde5028fd04882b22
erasherra <amir.ingher26@gmail.com>
2017-01-22 12:15:05 +0200

    Level2,Moonrotation

---
+++ Assets/script/SatelliteCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatelliteCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter2D(Collision2D other){
+
+		this.gameObject.SetActive (false);
+
+
+	}
+}
