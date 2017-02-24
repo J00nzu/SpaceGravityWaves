3827405242f77cc499566e1d63b63f97a07bee01
erasherra <amir.ingher26@gmail.com>
2017-02-24 17:23:06 +0200

    asteroidi liike, asteroidi from-to

---
+++ Assets/script/LevitationScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitationScript : MonoBehaviour {
+	// Use this for initialization
+
+	public float Div = 8;
+	public float dis = 0.2f;
+
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate(){
+		transform.position = new Vector3 (transform.position.x, Mathf.PingPong(Time.time/Div,dis));
+
+
+	}
+
+	// Update is called once per frame
+
+
+
+}
