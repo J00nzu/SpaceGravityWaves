a6beb1fdfdac6fe59f1fd49052cf81064b349264
J00nzu <j00nzuj@gmail.com>
2017-03-05 01:36:55 +0200

    Loading scene

--- Assets/LoadingScreen/Grabbable.cs
+++ Assets/LoadingScreen/Grabbable.cs
@@ -4,13 +4,4 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/LoadingScreen/LoaderScript.cs
+++ Assets/LoadingScreen/LoaderScript.cs
@@ -1,16 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoaderScript : MonoBehaviour {
 
+	Grabbable grabbed;
+	Vector3 offset;
+	AsyncOperation asyLoad;
+
+	public Image fadeOut;
+	public Image rotator;
+	public int levelIndexToLoad;
+
+	bool finalLoading;
+
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine ("LevelLoadRoutine");
 	}
-	
+
+	IEnumerator LevelLoadRoutine(){
+		fadeOut.CrossFadeAlpha (0, 6, true);
+
+		yield return new WaitForSeconds(3);
+
+		if (!Application.isEditor) {
+			asyLoad = SceneManager.LoadSceneAsync (levelIndexToLoad);
+		}
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (asyLoad != null) {
+			if (!finalLoading && asyLoad.progress>=0.8f) {
+				fadeOut.CrossFadeAlpha (1, 4, true);
+				finalLoading = true;
+			}
+		}
+
+		rotator.rectTransform.Rotate(new Vector3 (0, 0, -40) * Time.deltaTime);
+
+		if (grabbed == null) {
+			if (Input.GetMouseButton (0)) {
+				
+				RaycastHit hit; 
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+				if ( Physics.Raycast (ray,out hit,100.0f)) {
+					Grabbable grabo = hit.transform.GetComponent<Grabbable> ();
+					if (grabo != null) {
+						grabbed = grabo;
+						Vector3 off = grabo.transform.position - hit.point;
+						this.offset = new Vector3 (off.x, off.y, 0);
+
+						Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+						Debug.Log("Offset is " + offset); // ensure you picked right object
+
+					}
+				}
+			}
+		} else {
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			string[] layerNames = { "RaycastTarget" };
+			if (Physics.Raycast (ray, out hit, 100f, LayerMask.GetMask (layerNames))) {
+				Vector3 targetPosition = (new Vector3 (hit.point.x, hit.point.y, grabbed.transform.position.z) + offset);
+				Rigidbody rig = grabbed.GetComponent<Rigidbody> ();
+				rig.velocity += targetPosition - grabbed.transform.position;
+
+			}
+
+			if (!Input.GetMouseButton (0)) {
+				grabbed = null;
+			}
+		}
+
+
 	}
 }
