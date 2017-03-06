80f91b23e72335c52a8e4a1ccae8f8d60049bc8e
Yawnis <joonas.hall@gmail.com>
2017-03-06 12:49:54 +0200

    OSX fixes

---
+++ Assets/Editor/MacPlayerPrefs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MacPlayerPrefs : MonoBehaviour {
+
+	[MenuItem ("Edit/Reset Playerprefs")] public static void DeletePlayerPrefs() { PlayerPrefs.DeleteAll(); }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
